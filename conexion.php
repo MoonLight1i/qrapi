3192e01d0cd798501e5fbd770d270afea7da722a
MoonLight1i <90865727+MoonLight1i@users.noreply.github.com>
2024-07-15 16:51:56 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysql= new mysqli("34.16.51.17","root","Camila18.","testtrabajadores");
+$mysql= new mysqli("189.135.52.166"","root","Camila18.","testtrabajadores");
 if($mysql->connect_error){
     die("Error de conexion");
 }else{
