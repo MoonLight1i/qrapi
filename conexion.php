ab109ccfc13988eb6154611c5da2d044bad80fce
MoonLight1i <90865727+MoonLight1i@users.noreply.github.com>
2024-07-16 16:35:45 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbHost = "34.16.51.17"; // Puedes usar la IP de tu instancia de Cloud SQL
-$dbName = "testtrabajadores";
-$dbUser = "root";
-$dbPass = "Camila18.";
+$dbHost = "bnkmhfbejtjb7spgoo9y-mysql.services.clever-cloud.com"; // Puedes usar la IP de tu instancia de Cloud SQL
+$dbName = "bnkmhfbejtjb7spgoo9y";
+$dbUser = "u9vnum12sjerze2e";
+$dbPass = "mtFb4ZZOkfFc3ACB30W4";
 
 // Conexión a la base de datos
 $mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
