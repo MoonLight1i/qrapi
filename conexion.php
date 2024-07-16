1bf3bd728425116d12e3599fbedb42035171964f
MoonLight1i <90865727+MoonLight1i@users.noreply.github.com>
2024-07-16 11:51:52 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,20 @@
 <?php
 
-$mysql= new mysqli("34.16.51.17","root2","Camila18.","testtrabajadores", null , "helpful-binder-429515-u1:us-central1:qrdatabase");
-if($mysql->connect_error){
-    die("Error de conexion jijija");
-}else{
-    echo "conexion exitosa en google";
+$dbHost = "34.16.51.17"; // Puedes usar la IP de tu instancia de Cloud SQL
+$dbName = "testtrabajadores";
+$dbUser = "root";
+$dbPass = "Camila18.";
+
+// Conexión a la base de datos
+$mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+
+// Verifica la conexión
+if ($mysqli->connect_error) {
+    die("Error de conexión: " . $mysqli->connect_error);
+} else {
+    echo "Conexión exitosa a la base de datos en Google Cloud SQL";
 }
+
+// Cierra la conexión al finalizar el script (opcional)
+$mysqli->close();
+?>
