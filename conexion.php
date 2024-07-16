369b6d75c166dee7580d76374de44e712f0b663d
MoonLight1i <90865727+MoonLight1i@users.noreply.github.com>
2024-07-16 11:00:29 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysql= new mysqli("34.16.51.17","root2","Camila18.","testtrabajadores");
+$mysql= new mysqli("34.16.51.17","root2","Camila18.","testtrabajadores", null , "helpful-binder-429515-u1:us-central1:qrdatabase");
 if($mysql->connect_error){
     die("Error de conexion jijija");
 }else{
